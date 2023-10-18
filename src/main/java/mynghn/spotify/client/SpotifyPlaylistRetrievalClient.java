27a2461b46e784240ffea9ae8588c4622af336d3
Myounghoon Lee <l.mynghn@gmail.com>
2023-10-18 23:15:07 +0900

    :sparkles: Add GsonDecoder to SpotifyPlaylistRetrievalClient instance
    - Build Gson instance directly in order to register SpotifyPlaylistItemDeserializer
    - Other configurations are copied from OpenFeign's GsonFactory source

--- src/main/java/mynghn/spotify/client/SpotifyPlaylistRetrievalClient.java
+++ src/main/java/mynghn/spotify/client/SpotifyPlaylistRetrievalClient.java
@@ -1,15 +1,23 @@
 package mynghn.spotify.client;
 
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
 import feign.Feign;
+import feign.Headers;
 import feign.Param;
 import feign.QueryMap;
 import feign.Request;
 import feign.RequestLine;
+import feign.gson.DoubleToIntMapTypeAdapter;
+import feign.gson.GsonDecoder;
+import java.util.Map;
 import mynghn.common.auth.BearerAuthRequestInterceptor;
+import mynghn.spotify.deserializer.SpotifyPlaylistItemDeserializer;
 import mynghn.spotify.enums.BaseUrl;
 import mynghn.spotify.enums.EndPointTemplates;
 import mynghn.spotify.message.retrieval.request.query.SpotifyPlaylistRetrievalQueryParams;
 import mynghn.spotify.message.retrieval.response.SpotifyPlaylistRetrievalResponse;
+import mynghn.spotify.model.SpotifyPlaylistItem;
 
 public interface SpotifyPlaylistRetrievalClient {
 
@@ -21,9 +29,15 @@ public interface SpotifyPlaylistRetrievalClient {
      */
     static SpotifyPlaylistRetrievalClient connect(String accessToken) {
         return Feign.builder()
-                // TODO: Add response decoder
                 // TODO: Add error decoder
                 .requestInterceptor(new BearerAuthRequestInterceptor(accessToken))
+                .decoder(new GsonDecoder(new GsonBuilder()
+                        .setPrettyPrinting()
+                        .registerTypeAdapter(new TypeToken<Map<String, Object>>() {}.getType(),
+                                new DoubleToIntMapTypeAdapter())
+                        .registerTypeAdapter(SpotifyPlaylistItem.class,
+                                new SpotifyPlaylistItemDeserializer())
+                        .create()))
                 .options(new Request.Options())
                 .target(SpotifyPlaylistRetrievalClient.class, BaseUrl.WEB_API_V1.getValue());
     }
@@ -36,16 +50,17 @@ public interface SpotifyPlaylistRetrievalClient {
      * @return API response with playlist information
      */
     @RequestLine(EndPointTemplates.RETRIEVE_PLAYLIST)
-    SpotifyPlaylistRetrievalResponse fetch(@Param String id,
+    @Headers("Accept-Language: ko-KR,ko") // TODO: Support other languages
+    SpotifyPlaylistRetrievalResponse fetch(@Param("id") String id,
             @QueryMap SpotifyPlaylistRetrievalQueryParams params);
 
     /**
      * Retrieve Spotify playlist information (w/ default query parameter options)
      *
-     * @param id      Spotify ID of the playlist
+     * @param id Spotify ID of the playlist
      * @return API response with playlist information
      */
     default SpotifyPlaylistRetrievalResponse fetch(String id) {
-        return fetch(id, SpotifyPlaylistRetrievalQueryParams.empty());
+        return fetch(id, SpotifyPlaylistRetrievalQueryParams.buildDefault());
     }
 }
