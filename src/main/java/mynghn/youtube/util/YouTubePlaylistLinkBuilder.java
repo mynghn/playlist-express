732a4200843a15e36b1e8f4859d878c97b216c1c
Myounghoon Lee <l.mynghn@gmail.com>
2023-11-11 17:59:27 +0900

    :recycle: Pass YouTubePlaylist obj to YouTubePlaylistLinkBuilder

--- src/main/java/mynghn/youtube/util/YouTubePlaylistLinkBuilder.java
+++ src/main/java/mynghn/youtube/util/YouTubePlaylistLinkBuilder.java
@@ -1,11 +1,12 @@
 package mynghn.youtube.util;
 
+import mynghn.youtube.model.YouTubePlaylist;
+
 public class YouTubePlaylistLinkBuilder {
 
     private static final String LINK_TEMPLATE = "https://www.youtube.com/playlist?list=%s";
 
-    public static String build(String playlistId) {
-        return String.format(LINK_TEMPLATE, playlistId);
+    public static String build(YouTubePlaylist playlist) {
+        return String.format(LINK_TEMPLATE, playlist.id());
     }
-
 }
